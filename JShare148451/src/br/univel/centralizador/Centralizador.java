9ba5a2e003112d62b24305655121322253e99d37
Semicheche <lucianosemicheche@gmail.com>
2016-04-12 00:44:39 -0300

    criação do metodo para pegar os IP´s da placa de rede

--- JShare148451/src/br/univel/centralizador/Centralizador.java
+++ JShare148451/src/br/univel/centralizador/Centralizador.java
@@ -16,7 +16,12 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 
@@ -25,15 +30,16 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JComboBox;
 
-public class Centralizador extends JFrame implements IServer{
+public class Centralizador extends JFrame implements IServer {
 
 	private JPanel contentPane;
-	private JTextField txtip;
 	private JTextField txtporta;
 	private JTextArea txtarea;
 	private JButton btnIniciarServico;
 	private JButton btnPararServico;
+	private JComboBox comboip;
 
 	/**
 	 * Launch the application.
@@ -44,6 +50,9 @@ public class Centralizador extends JFrame implements IServer{
 				try {
 					Centralizador frame = new Centralizador();
 					frame.setVisible(true);
+
+					frame.configurar();
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -61,16 +70,16 @@ public class Centralizador extends JFrame implements IServer{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.NORTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{40, 159, 44, 93, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 40, 159, 44, 93, 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JLabel lblIp = new JLabel("IP:");
 		lblIp.setFont(new Font("Tahoma", Font.BOLD, 12));
 		GridBagConstraints gbc_lblIp = new GridBagConstraints();
@@ -79,16 +88,15 @@ public class Centralizador extends JFrame implements IServer{
 		gbc_lblIp.gridx = 0;
 		gbc_lblIp.gridy = 0;
 		panel.add(lblIp, gbc_lblIp);
-		
-		txtip = new JTextField();
-		GridBagConstraints gbc_txtip = new GridBagConstraints();
-		gbc_txtip.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtip.insets = new Insets(0, 0, 0, 5);
-		gbc_txtip.gridx = 1;
-		gbc_txtip.gridy = 0;
-		panel.add(txtip, gbc_txtip);
-		txtip.setColumns(10);
-		
+
+		comboip = new JComboBox();
+		GridBagConstraints gbc_comboip = new GridBagConstraints();
+		gbc_comboip.insets = new Insets(0, 0, 0, 5);
+		gbc_comboip.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboip.gridx = 1;
+		gbc_comboip.gridy = 0;
+		panel.add(comboip, gbc_comboip);
+
 		JLabel lblPorta = new JLabel("Porta:");
 		lblPorta.setFont(new Font("Tahoma", Font.BOLD, 12));
 		GridBagConstraints gbc_lblPorta = new GridBagConstraints();
@@ -97,7 +105,7 @@ public class Centralizador extends JFrame implements IServer{
 		gbc_lblPorta.gridx = 2;
 		gbc_lblPorta.gridy = 0;
 		panel.add(lblPorta, gbc_lblPorta);
-		
+
 		txtporta = new JTextField();
 		GridBagConstraints gbc_txtporta = new GridBagConstraints();
 		gbc_txtporta.fill = GridBagConstraints.HORIZONTAL;
@@ -106,47 +114,86 @@ public class Centralizador extends JFrame implements IServer{
 		gbc_txtporta.gridy = 0;
 		panel.add(txtporta, gbc_txtporta);
 		txtporta.setColumns(10);
-		
+
 		btnIniciarServico = new JButton("Iniciar Serviço");
 		GridBagConstraints gbc_btnIniciarServico = new GridBagConstraints();
 		gbc_btnIniciarServico.insets = new Insets(0, 0, 0, 5);
 		gbc_btnIniciarServico.gridx = 4;
 		gbc_btnIniciarServico.gridy = 0;
 		panel.add(btnIniciarServico, gbc_btnIniciarServico);
-		
+
 		btnPararServico = new JButton("Parar Serviço");
 		GridBagConstraints gbc_btnPararServico = new GridBagConstraints();
 		gbc_btnPararServico.gridx = 5;
 		gbc_btnPararServico.gridy = 0;
 		panel.add(btnPararServico, gbc_btnPararServico);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
-		
+
 		txtarea = new JTextArea();
 		txtarea.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		txtarea.setBackground(Color.WHITE);
 		scrollPane.setViewportView(txtarea);
 	}
-	
-	
-/**===========================================================================
- *
- * implemetação dos metodos da Interface
- * 
- * ===========================================================================
- */
-	
+
+	protected void configurar() {
+
+		btnIniciarServico.setEnabled(false);
+
+		List<String> lista = getIpDisponivel();
+
+	}
+
+	private List<String> getIpDisponivel() {
+
+		List<String> listIp = new ArrayList<String>();
+
+		try {
+			Enumeration<NetworkInterface> iface = NetworkInterface.getNetworkInterfaces();
+
+			while (iface.hasMoreElements()) {
+				NetworkInterface netifc = iface.nextElement();
+				if (netifc.isUp()) {
+					Enumeration<InetAddress> addresses = netifc.getInetAddresses();
+					while (addresses.hasMoreElements()) {
+
+						InetAddress addr = addresses.nextElement();
+
+						String ip = addr.getHostAddress();
+
+						if (ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+							listIp.add(ip);
+						}
+					}
+				}
+
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		return listIp;
+	}
+
+	/**
+	 * =========================================================================
+	 *
+	 * implemetação dos metodos da Interface
+	 * 
+	 * =========================================================================
+	 */
+
 	@Override
 	public void registrarCliente(Cliente c) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -164,7 +211,7 @@ public class Centralizador extends JFrame implements IServer{
 	@Override
 	public void desconectar(Cliente c) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
